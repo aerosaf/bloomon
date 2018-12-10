b0dfc5484ed9352c9020894ba1ab72ec3123b25b
Aerosaf <aerosaf@gmail.com>
2018-12-10 18:01:02 +0800

    Bouquets algorithem

--- src/FlowerStoreCommand.php
+++ src/FlowerStoreCommand.php
@@ -3,12 +3,17 @@
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 Class FlowerStoreCommand extends Command {
 
     public $bouquetSpecs = [];
     public $flowers = [];
-    
+    public $flowerGroup = [];
+    public $bouquets = [];
+    public $cycle = 0;
+
     public function configure()
     {
         $this->setName('total')
@@ -17,8 +22,11 @@ Class FlowerStoreCommand extends Command {
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
+        // Reads the input in the sample folder.
+        $this->readFile('./sample/input.txt');
+
         // Output options
-        $output->writeln("Hello");
+        $output->writeln($this->bouquets($output));
     }
 
     public function unitTesting($input)
@@ -48,5 +56,110 @@ Class FlowerStoreCommand extends Command {
             return 'Error opening file.';
         } 
         $this->flowerGroup = array_count_values($this->flowers);
+    }
+
+
+    public function bouquets($output)
+    {
+        // Go through all bouquet spec untill no bouquet can be found in a cycle
+        while ($this->getBouquet() == 1) {
+            $this->cycle++;
+        }
+
+        $output = $this->getOutput();
+
+        return $output;
+    }
+
+    public function getBouquet()
+    {
+        $result = 0;
+
+        foreach($this->bouquetSpecs as $bouquetSpec) {
+            
+            // Get bouquet size
+            preg_match('/(?<=[A-Z])L|S(?=\d)/', $bouquetSpec, $bouquetSize);
+            
+            // Get bouquet rules
+            preg_match_all('/\d+[a-z]/', $bouquetSpec, $bouquetRules);
+            
+            // Get Bouquets from input
+            foreach($bouquetRules as $rules) {
+                if($this->getFlowers($bouquetSize[0], $rules) == 1){
+
+                    // Set one bouquet every cycle found
+                    if(!isset($this->bouquets[$bouquetSpec])){
+                        $this->bouquets[$bouquetSpec] = 1;
+                    } else {
+                        $this->bouquets[$bouquetSpec]++;
+                    }
+                    $result = 1;
+                } else {
+                    if(!isset($this->bouquets[$bouquetSpec])){
+                        $this->bouquets[$bouquetSpec] = 0;
+                    }
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    public function getFlowers($bouquetSize, $rules)
+    {
+        $haveFlower = 0;
+       
+        // Run through all the flower in the specs
+        foreach($rules as $rule) {
+
+            // Seporate the flower name from the quantity
+            $rule = preg_split('/(?<=[0-9])(?=[a-z]+)/i',$rule);
+
+            // Look for flowers
+            $foundFlower = $this->matchFlowers($bouquetSize, $rule);
+
+            // Stop search for the rest of the spec if flower is not found
+            if($foundFlower == 0){
+                break;
+            } else {
+                $haveFlower = 1;
+            }
+        }
+
+        return $haveFlower;
+    }
+
+    public function getOutput()
+    {
+        $output = "<comment>Created bouquets</comment>\n";
+        foreach($this->bouquets as $bouquet => $quantity) {
+            $output .= $bouquet . " : <info>" . $quantity . "</info>\n";
+        }
+
+        $output .= "\n<comment>With leftover flowers</comment>\n";
+
+        foreach($this->flowerGroup as $flower => $quantity) {
+            $output .= $flower . " : <info>" . $quantity . "</info>\n";
+        }
+
+        return $output;
+    }
+
+    public function matchFlowers($bouquetSize, $rule)
+    {
+        // Go through the input to macth found flowers
+        $foundFlower = 0;
+        foreach($this->flowerGroup as $flower => $quantity) {
+            $flowerSize = substr($flower, -1);
+            $flowerName = substr($flower, 0, -1);
+            
+            // Deduct flowers from from input used for the bouquet
+            if($flowerSize == $bouquetSize && $flowerName == $rule[1] && $quantity >= $rule[0]) {
+                $this->flowerGroup[$flower] = $quantity - $rule[0];
+                $foundFlower = 1;
+            }
+        }
+
+        return $foundFlower;
     }
 }
--- tests/FlowerStoreCommandTest.php
+++ tests/FlowerStoreCommandTest.php
@@ -5,6 +5,13 @@ use Aero\FlowerStoreCommand;
 
 class FlowerStoreCommandTest extends TestCase {
 
+    public $FlowerStoreCommand;
+
+    function setUp() {
+        $this->FlowerStoreCommand = new FlowerStoreCommand;
+        $this->FlowerStoreCommand->readFile('./sample/input.txt');		
+    }
+
     public function testInitialOutput(): void
     {
         $FlowerStoreCommand = new FlowerStoreCommand;
@@ -20,4 +27,36 @@ class FlowerStoreCommandTest extends TestCase {
         $this->assertDirectoryExists('./sample/');
     }
 
+    public function testCanFindFlower(): void
+    {
+        $this->assertEquals(
+            1,
+            $this->FlowerStoreCommand->getFlowers('L', ['10a' ,'15b' ,'5c'])
+        );
+    }
+
+    public function testCanMakeBouquet(): void
+    {
+        $this->assertEquals(
+            1,
+            $this->FlowerStoreCommand->getBouquet()
+        );
+    }
+
+    public function testFindFlowerThatMatchTheSpecAndDeductFromStorage(): void
+    {
+        $flowerGroup_aL = $this->FlowerStoreCommand->flowerGroup['aL'];
+            
+        $this->assertEquals(
+            1,
+            $this->FlowerStoreCommand->matchFlowers('L', [10, 'a'])
+        );
+
+        $flowerGroup_aL_after_assert = $this->FlowerStoreCommand->flowerGroup['aL'];
+
+        $this->assertEquals(
+            $flowerGroup_aL-10,
+            $flowerGroup_aL_after_assert
+        );
+    }
 }
